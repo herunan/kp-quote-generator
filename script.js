7d2a789b254a068ee185b57e9de8ef42703e4304
herunan <hernrome@gmail.com>
2017-04-19 02:47:18 +0100

    Added tweet array and prolonged initial trigger

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 $(document).ready(function() {
+
     setTimeout(function() {
     $("#generator").trigger("click");
-},10);
+},1000);
 
-    var quotes = [{
-            Quote: "Text 1",
-            Person: "Karl Pilkington"
-        },
-        {
-            Quote: "Text 2",
-            Person: "Karl Pilkington"
-        },
-        {
-            Quote: "Text 3",
-            Person: "Karl Pilkington"
-        },
-    ];
-    $("#generator").on("click", function() {
-        var randomNumber = Math.floor(Math.random() * quotes.length);
-        $("#quote").html(Object.values("“" + quotes[randomNumber].Quote + "”"));
-        $("#person").html(Object.values(" – " + quotes[randomNumber].Person));
-    });
-    $("#tweet-button").click(function() { // create if alert for when text is not generated OR generate text on load
+$("#generator").on("click", function() {
+    var randomN = Math.floor(Math.random() * tweetArr.length);
+    $("#quote").html(Object.values("“" + tweetArr[randomN] + "”"));
+});
+
+    $("#tweet-button").click(function() {
         var quoteText = document.getElementById("quote-area").innerText;
-        $(".tweet").attr("href", "https://twitter.com/intent/tweet?text=" + quoteText + "%23KPquotes");
+        $(".tweet").attr("href", "https://twitter.com/intent/tweet?text=" + quoteText + " %23KPquotes");
     });
 });
